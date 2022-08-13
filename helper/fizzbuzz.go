5d8eccd7431aa15e81026f560c50e888d88fe7f3
nang-dwirobbi <dwirobbi291@gmail.com>
2022-08-13 15:04:53 +0700

    add fizzbuzz.go file to helper/

---
+++ helper/fizzbuzz.go
@@ -0,0 +1,36 @@
+package helper
+
+// package main
+
+import (
+	// "fmt"
+	"strconv"
+)
+
+func FizzBuzz(from, to int) string {
+	fizz := "Fizz"
+	buzz := "Buzz"
+
+	for ; from <= to; from++ {
+		if from%15 == 0 {
+			return fizz + buzz
+		} else if from%5 == 0 {
+			return buzz
+		} else if from%3 == 0 {
+			return fizz
+		} else {
+			return strconv.Itoa(from)
+		}
+	}
+
+	return ""
+}
+
+// =====Run Program Without Unit Test=====
+// func main() {
+// 	var from, to = 1, 100
+
+// 	for ; from <= to; from++ {
+// 		fmt.Println(FizzBuzz(from, to))
+// 	}
+// }
